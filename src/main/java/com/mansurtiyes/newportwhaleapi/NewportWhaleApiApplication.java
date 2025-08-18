ee4161d5f2ffc12d843c9ed4c738e58496628358
Mansur Tiyes <tiyes@usc.edu>
2025-08-19 00:56:24 +0500

    fix: Enabled scheduling in NewportWhaleApiApplication 0.0.19

--- src/main/java/com/mansurtiyes/newportwhaleapi/NewportWhaleApiApplication.java
+++ src/main/java/com/mansurtiyes/newportwhaleapi/NewportWhaleApiApplication.java
@@ -2,8 +2,10 @@ package com.mansurtiyes.newportwhaleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NewportWhaleApiApplication {
 
     public static void main(String[] args) {
